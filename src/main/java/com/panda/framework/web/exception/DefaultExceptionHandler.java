ddb7a9132f2ff962266f428f9eb8cec5e1f625da
Jamie <884013178@qq.com>
2019-01-09 16:33:00 +0800

    添加自定义异常处理

--- src/main/java/com/panda/framework/web/exception/DefaultExceptionHandler.java
+++ src/main/java/com/panda/framework/web/exception/DefaultExceptionHandler.java
@@ -1,9 +1,10 @@
-package com.panda.framework.exception;
+package com.panda.framework.web.exception;
 
 
 import com.panda.framework.domain.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +19,17 @@ public class DefaultExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(DefaultExceptionHandler.class);
 
+
+    /**
+     * 请求方式不支持
+     */
+    @ExceptionHandler({ HttpRequestMethodNotSupportedException.class })
+    public AjaxResult handleException(HttpRequestMethodNotSupportedException e)
+    {
+        log.error(e.getMessage(), e);
+        return AjaxResult.error("不支持' " + e.getMethod() + "'请求");
+    }
+
     /**
      * 拦截未知的运行时异常
      */
@@ -25,9 +37,18 @@ public class DefaultExceptionHandler {
     public AjaxResult notFount(RuntimeException e)
     {
         log.error("运行时异常:", e);
-        return AjaxResult.error("运行时异常:" + e.getMessage());
+        return AjaxResult.error(e.getMessage());
     }
 
+    /**
+     * 系统异常
+     */
+    @ExceptionHandler(Exception.class)
+    public AjaxResult handleException(Exception e)
+    {
+        log.error(e.getMessage(), e);
+        return AjaxResult.error("服务器错误，请联系管理员");
+    }
 
 
 
--- src/main/java/com/panda/project/system/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/panda/project/system/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.panda.project.system.user.service.impl;
 
+import com.panda.common.exception.base.BaseException;
+import com.panda.common.exception.user.CaptchaException;
 import com.panda.project.system.user.domain.User;
 import com.panda.project.system.user.mapper.UserMapper;
 import com.panda.project.system.user.service.UserService;
@@ -23,6 +25,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getAllUser() {
 
-        return userMapper.getAllUser();
+        List<User> user = userMapper.getAllUser();
+        String stringArray[] = {"微学苑", "http://www.weixueyuan.net", "一切编程语言都是纸老虎"};
+
+        throw new BaseException("1000",stringArray);
+//        return user;
     }
 }
