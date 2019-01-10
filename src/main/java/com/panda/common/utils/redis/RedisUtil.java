d04cb37f6607c1a22dd69525722a139d9a19af0c
Jamie <884013178@qq.com>
2019-01-10 12:14:42 +0800

    提取Redis工具类接口

--- src/main/java/com/panda/common/utils/redis/RedisUtil.java
+++ src/main/java/com/panda/common/utils/redis/RedisUtil.java
@@ -46,8 +46,9 @@ public class RedisUtil implements RedisDao{
      * 根据key 获取过期时间
      *
      * @param key 键 不能为null
-     * @return 时间(秒) 返回0代表为永久有效
+     * @return 时间(秒) 返回-1代表为永久有效
      */
+    @Override
     public long getExpire(String key) {
         return redisTemplate.getExpire(key, TimeUnit.SECONDS);
     }
@@ -58,6 +59,7 @@ public class RedisUtil implements RedisDao{
      * @param key 键
      * @return true 存在 false不存在
      */
+    @Override
     public boolean hasKey(String key) {
         try {
             return redisTemplate.hasKey(key);
@@ -73,6 +75,7 @@ public class RedisUtil implements RedisDao{
      * @param key 可以传一个值 或多个
      */
     @SuppressWarnings("unchecked")
+    @Override
     public void del(String... key) {
         if (key != null && key.length > 0) {
             if (key.length == 1) {
@@ -143,6 +146,7 @@ public class RedisUtil implements RedisDao{
      * @param delta 要增加几(大于0)
      * @return
      */
+    @Override
     public long incr(String key, long delta) {
         if (delta < 0) {
             throw new RuntimeException("递增因子必须大于0");
@@ -157,6 +161,7 @@ public class RedisUtil implements RedisDao{
      * @param delta 要减少几(小于0)
      * @return
      */
+    @Override
     public long decr(String key, long delta) {
         if (delta < 0) {
             throw new RuntimeException("递减因子必须大于0");
@@ -172,6 +177,7 @@ public class RedisUtil implements RedisDao{
      * @param item 项 不能为null
      * @return 值
      */
+    @Override
     public Object hget(String key, String item) {
         return redisTemplate.opsForHash().get(key, item);
     }
@@ -182,6 +188,7 @@ public class RedisUtil implements RedisDao{
      * @param key 键
      * @return 对应的多个键值
      */
+    @Override
     public Map<Object, Object> hmget(String key) {
         return redisTemplate.opsForHash().entries(key);
     }
@@ -193,6 +200,7 @@ public class RedisUtil implements RedisDao{
      * @param map 对应多个键值
      * @return true 成功 false 失败
      */
+    @Override
     public boolean hmset(String key, Map<String, Object> map) {
         try {
             redisTemplate.opsForHash().putAll(key, map);
@@ -211,6 +219,7 @@ public class RedisUtil implements RedisDao{
      * @param time 时间(秒)
      * @return true成功 false失败
      */
+    @Override
     public boolean hmset(String key, Map<String, Object> map, long time) {
         try {
             redisTemplate.opsForHash().putAll(key, map);
@@ -232,6 +241,7 @@ public class RedisUtil implements RedisDao{
      * @param value 值
      * @return true 成功 false失败
      */
+    @Override
     public boolean hset(String key, String item, Object value) {
         try {
             redisTemplate.opsForHash().put(key, item, value);
@@ -251,6 +261,7 @@ public class RedisUtil implements RedisDao{
      * @param time  时间(秒) 注意:如果已存在的hash表有时间,这里将会替换原有的时间
      * @return true 成功 false失败
      */
+    @Override
     public boolean hset(String key, String item, Object value, long time) {
         try {
             redisTemplate.opsForHash().put(key, item, value);
@@ -270,6 +281,7 @@ public class RedisUtil implements RedisDao{
      * @param key  键 不能为null
      * @param item 项 可以使多个 不能为null
      */
+    @Override
     public void hdel(String key, Object... item) {
         redisTemplate.opsForHash().delete(key, item);
     }
@@ -281,6 +293,7 @@ public class RedisUtil implements RedisDao{
      * @param item 项 不能为null
      * @return true 存在 false不存在
      */
+    @Override
     public boolean hHasKey(String key, String item) {
         return redisTemplate.opsForHash().hasKey(key, item);
     }
@@ -293,6 +306,7 @@ public class RedisUtil implements RedisDao{
      * @param by   要增加几(大于0)
      * @return
      */
+    @Override
     public double hincr(String key, String item, double by) {
         return redisTemplate.opsForHash().increment(key, item, by);
     }
@@ -305,6 +319,7 @@ public class RedisUtil implements RedisDao{
      * @param by   要减少记(小于0)
      * @return
      */
+    @Override
     public double hdecr(String key, String item, double by) {
         return redisTemplate.opsForHash().increment(key, item, -by);
     }
@@ -316,6 +331,7 @@ public class RedisUtil implements RedisDao{
      * @param key 键
      * @return
      */
+    @Override
     public Set<Object> sGet(String key) {
         try {
             return redisTemplate.opsForSet().members(key);
@@ -332,6 +348,7 @@ public class RedisUtil implements RedisDao{
      * @param value 值
      * @return true 存在 false不存在
      */
+    @Override
     public boolean sHasKey(String key, Object value) {
         try {
             return redisTemplate.opsForSet().isMember(key, value);
@@ -348,6 +365,7 @@ public class RedisUtil implements RedisDao{
      * @param values 值 可以是多个
      * @return 成功个数
      */
+    @Override
     public long sSet(String key, Object... values) {
         try {
             return redisTemplate.opsForSet().add(key, values);
@@ -365,6 +383,7 @@ public class RedisUtil implements RedisDao{
      * @param values 值 可以是多个
      * @return 成功个数
      */
+    @Override
     public long sSetAndTime(String key, long time, Object... values) {
         try {
             Long count = redisTemplate.opsForSet().add(key, values);
@@ -383,6 +402,7 @@ public class RedisUtil implements RedisDao{
      * @param key 键
      * @return
      */
+    @Override
     public long sGetSetSize(String key) {
         try {
             return redisTemplate.opsForSet().size(key);
@@ -399,6 +419,7 @@ public class RedisUtil implements RedisDao{
      * @param values 值 可以是多个
      * @return 移除的个数
      */
+    @Override
     public long setRemove(String key, Object... values) {
         try {
             Long count = redisTemplate.opsForSet().remove(key, values);
@@ -418,6 +439,7 @@ public class RedisUtil implements RedisDao{
      * @param end   结束 0 到 -1代表所有值
      * @return
      */
+    @Override
     public List<Object> lGet(String key, long start, long end) {
         try {
             return redisTemplate.opsForList().range(key, start, end);
@@ -433,6 +455,7 @@ public class RedisUtil implements RedisDao{
      * @param key 键
      * @return
      */
+    @Override
     public long lGetListSize(String key) {
         try {
             return redisTemplate.opsForList().size(key);
@@ -449,6 +472,7 @@ public class RedisUtil implements RedisDao{
      * @param index 索引 index>=0时， 0 表头，1 第二个元素，依次类推；index<0时，-1，表尾，-2倒数第二个元素，依次类推
      * @return
      */
+    @Override
     public Object lGetIndex(String key, long index) {
         try {
             return redisTemplate.opsForList().index(key, index);
@@ -463,9 +487,9 @@ public class RedisUtil implements RedisDao{
      *
      * @param key   键
      * @param value 值
-     * @param time  时间(秒)
      * @return
      */
+    @Override
     public boolean lSet(String key, Object value) {
         try {
             redisTemplate.opsForList().rightPush(key, value);
@@ -484,6 +508,7 @@ public class RedisUtil implements RedisDao{
      * @param time  时间(秒)
      * @return
      */
+    @Override
     public boolean lSet(String key, Object value, long time) {
         try {
             redisTemplate.opsForList().rightPush(key, value);
@@ -501,9 +526,9 @@ public class RedisUtil implements RedisDao{
      *
      * @param key   键
      * @param value 值
-     * @param time  时间(秒)
      * @return
      */
+    @Override
     public boolean lSet(String key, List<Object> value) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
@@ -522,6 +547,7 @@ public class RedisUtil implements RedisDao{
      * @param time  时间(秒)
      * @return
      */
+    @Override
     public boolean lSet(String key, List<Object> value, long time) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
@@ -542,6 +568,7 @@ public class RedisUtil implements RedisDao{
      * @param value 值
      * @return
      */
+    @Override
     public boolean lUpdateIndex(String key, long index, Object value) {
         try {
             redisTemplate.opsForList().set(key, index, value);
@@ -560,6 +587,7 @@ public class RedisUtil implements RedisDao{
      * @param value 值
      * @return 移除的个数
      */
+    @Override
     public long lRemove(String key, long count, Object value) {
         try {
             Long remove = redisTemplate.opsForList().remove(key, count, value);
